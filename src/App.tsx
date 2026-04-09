655a9b2d9dfb39d78d5317b929dab399f7789fae
Joe <alvesoscar517@gmail.com>
2026-04-09 21:45:47 +0700

    Initial commit: Notes app with Tiptap

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SimpleEditor } from "./components/tiptap-templates/simple/simple-editor"
+
+function App() {
+  return (
+    <main className="w-full h-screen bg-white dark:bg-zinc-950">
+      <SimpleEditor />
+    </main>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/Simple-editor/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+})
